b8677fe10e92671e4f59f7f1785ed8678ae21873
Noémi Nagygéci <nagyg.emi@gmail.com>
2022-01-05 14:26:51 +0100

    Feladat

--- Desktop/src/Vizsgaremek/Models/ProgramInfo.cs
+++ Desktop/src/Vizsgaremek/Models/ProgramInfo.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Octokit;
+using System.Diagnostics;
+
+
 using System.Reflection;
 
 namespace Vizsgaremek.Models
@@ -40,6 +44,8 @@ namespace Vizsgaremek.Models
 
             foreach (Attribute attr in Attribute.GetCustomAttributes(assembly))
             {
+                GetGithubCollaboratorsName();
+
                 if (attr.GetType() == typeof(AssemblyTitleAttribute))
                     title = ((AssemblyTitleAttribute)attr).Title;
                 else if (attr.GetType() == typeof(AssemblyDescriptionAttribute))
@@ -50,5 +56,31 @@ namespace Vizsgaremek.Models
             }
 
         }
+
+        private async void GetGithubCollaboratorsName()
+        {
+            string reponame = "NagygeciNoemi";
+            int repoId = 434305942;
+            var client = new GitHubClient(new ProductHeaderValue(reponame));
+
+            // fejlesztők meghatározása
+            try
+            {
+                var collaborators = await client.Repository.GetAllContributors(repoId);
+                string collaboratorsName = string.Empty;
+                foreach (var collaborator in collaborators)
+                {
+                    string collaboratorLoginName = collaborator.Login;
+                    var user = await client.User.Get(collaboratorLoginName);
+                    collaboratorsName += user.Name + " (" + user.Login + ") ";
+                }
+                authors = collaboratorsName;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+
     }
 }
--- Desktop/src/Vizsgaremek/Pages/ProgramVersion.xaml.cs
+++ Desktop/src/Vizsgaremek/Pages/ProgramVersion.xaml.cs
@@ -39,5 +39,10 @@ namespace Vizsgaremek.Pages
             // Statikus osztály ezért az osztály nevét írjuk
             Navigate.Navigation(welcomePage);
         }
+
+        private void btAuthors_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
